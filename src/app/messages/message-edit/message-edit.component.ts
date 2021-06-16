3a4d3c5922528bd6743b875472329a236cba3266
rjdaw <daw19003@byui.edu>
2021-06-16 14:46:02 -0600

    finishing week 3

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent implements OnInit {
+  currentSender = "Ruben";
+  @ViewChild('subject') subjectRef: ElementRef;
+  @ViewChild('msgText') msgTextRef: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSendMessage() {
+    const newSubject = this.subjectRef.nativeElement.value;
+    const newMsgText = this.msgTextRef.nativeElement.value;
+    const newMsg = new Message(1, newSubject, newMsgText, this.currentSender);
+    this.addMessageEvent.emit(newMsg);
+  }
+
+  onClear() {
+    this.subjectRef.nativeElement.value = "";
+    this.msgTextRef.nativeElement.value = "";
   }
 
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-list.component.css']
 })
 export class MessageListComponent implements OnInit {
+  messages: Message[] = [
+    new Message(1, "TA Position", "I'm super glad to be your TA this semester!", "Roger"),
+    new Message(2, "Pizza Party", "I'm really happy that we will have a pizza party every day until the end of the semester.", "John")
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddMessage(message: Message) {
+    this.messages.push(message);
   }
 
 }
